3416b64cfbf00b40c8ac8fdad13985d152028a6c
Crystal Vision <117869099+sonalbirari@users.noreply.github.com>
2023-08-15 16:57:16 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "sonal-aws-tf-state-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
